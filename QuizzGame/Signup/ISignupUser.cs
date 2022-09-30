14245d9b5cf6ac0d94dae73263affe8e99e65409
OSgameDev <mrwow.wow2021@gmail.com>
2022-09-30 04:55:23 +0100

    new#

---
+++ QuizzGame/Signup/ISignupUser.cs
@@ -0,0 +1,10 @@
+﻿
+namespace QuizGame.Signup
+{
+    public interface ISignupUser
+    {
+        void AddUser(string username, string password);
+        bool IsSuccess();
+        string ErrorMessage();
+    }
+}
